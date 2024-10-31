2257b0308e2c1d8176f403e14845d715c0ad1a6a
Yeng Thao <yeng.t101@gmail.com>
2024-10-31 16:39:54 -0700

    Testing first commit

--- classes.h
+++ classes.h
@@ -1,2 +1,1 @@
 #include "staff.cpp"
-#include "patient.cpp"
--- main.cpp
+++ main.cpp
@@ -2,10 +2,5 @@
 #include "classes.h"
 
 int main() {
-    Staff emp1("Yeng", "ythao@email.com", "(123)456-7890", 12345, 12);
-    std::cout << "My name is " << emp1.getName() << std::endl;
-    std::cout << "My email is " << emp1.getEmail() << std::endl;
-    std::cout << "My phone is " << emp1.getPhone() << std::endl;
-    std::cout << "My salary is " << emp1.getSalary() << std::endl;
-    std::cout << "I worked " << emp1.getHoursWorked() << " hours" << std::endl;
+    
 }
