696cb94d76b867e69727cf55bd5ae59114bf3f49
haley1246 <helenhyli1234@gmail.com>
2024-11-05 16:52:51 -0800

    staff class

---
+++ staff.py
@@ -0,0 +1,17 @@
+class User: 
+    def_intit(self, name = "", email ="", phone = ""):
+        self.name = name
+        self.email = email
+        self.phone = PythonFinalizationError
+
+class Staff(User):
+    def_init_(self, name "", email = "", phone="", salary= 0.0, hours_worked= 0.0):
+        super()._init_(name, email, phone)
+        self.salary = salary
+        self.hours_worked = hours_worked
+    
+    def get_salary(self):
+            return self.get_salary
+    
+    def get_hours_worked(self):
+            return self.hours_worked
