d0fec14dc889a21b014911be836faeeecea65c97
Nayeli Flores Valdez <floresvaldeznayeli@gmail.comm>
2024-11-10 16:38:46 -0800

    User Class

---
+++ user.py
@@ -0,0 +1,25 @@
+class User:
+
+    def __init__(self, name, email, phoneNumber):
+        self.name = name
+        self.email = email
+        self.phoneNumber = phoneNumber
+
+    def getName(self):
+        return self.name
+    
+    def getEmail(self):
+        return self.email
+    
+    def getPhoneNumber(self):
+        return self.phoneNumber
+    
+    # I put 'pass' as a placeholder 
+    def signup():
+        pass
+
+    def login():
+        pass
+
+    def logout():
+        pass
