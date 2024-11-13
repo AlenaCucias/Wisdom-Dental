ece7c8e49d879391322a956e9b21a52a49f69da8
MadelynAnne <madelynannekirkman@csus.edu>
2024-11-12 21:32:32 -0800

    adding front end

---
+++ wisdomdental/src/App.js
@@ -0,0 +1,17 @@
+import HomePage from './Pages/HomePage';
+import Header from './components/Header';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+        <Header />
+        <Routes>
+          <Route path = '/' element={<HomePage />} />
+        </Routes>
+    </div>
+  );
+}
+
+export default App;
