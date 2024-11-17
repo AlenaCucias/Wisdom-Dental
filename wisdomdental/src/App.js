8c59a7d1f832f72a917502d1ece8af320a858870
MadelynAnne <madelynannekirkman@csus.edu>
2024-11-16 20:08:39 -0800

    fixing app.js

--- wisdomdental/src/App.js
+++ wisdomdental/src/App.js
@@ -1,6 +1,5 @@
 import HomePage from './Pages/HomePage';
 import Header from './components/Header';
-//import FeedbackPage from './FeedbackPageCopy';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import LoginPage from './Pages/LoginPage';
