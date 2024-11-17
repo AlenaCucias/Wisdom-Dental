91988936860f7dd4cf08db677babf6da6e016740
MadelynAnne <madelynannekirkman@csus.edu>
2024-11-17 00:26:15 -0800

    added routing to the feedback page

--- wisdomdental/src/App.js
+++ wisdomdental/src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import LoginPage from './Pages/LoginPage';
+import FeedbackPage from './Pages/FeedbackPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path = '/' element={<HomePage />} />
           <Route path='login' element={<LoginPage />} />
+          <Route path='feedback' element={<FeedbackPage />} />
         </Routes>
     </div>
   );
