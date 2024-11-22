19837811bd62e9bc7309bcff0382338caf58e140
MadelynAnne <madelynannekirkman@csus.edu>
2024-11-21 18:45:45 -0800

    small changes to homepage

--- wisdomdental/src/Pages/HomePage.js
+++ wisdomdental/src/Pages/HomePage.js
@@ -11,117 +11,131 @@ import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   return (
-    <div className='home-page mt-5'>
-      <Row className='mb-5'>
-        <Col md="5" className='my-auto pe-4'>
-          <div className="image-container">
-            <img
-              className="screenshot"
-              alt="logo"
-              src={logo}
-            />
-          </div>
-        </Col>
-        <Col md="7" className='my-auto'>
-          <Row>
-            <div className="text-wrapper text-start pb-1">Welcome to</div>
+    <div className='home-page mt-1'>
+      <Row className="align-items-stretch">
+        <Col md='6'>     
+          <Row className='h-50 align-items-center'>
+            <Col className='d-flex justify-content-center'>
+              <div className="image-container">
+                <img
+                  className="screenshot"
+                  alt="logo"
+                  src={logo}
+                />
+              </div>
+              </Col>
           </Row>
-          <Row>
-            <div className="div gruppo-regular text-start pb-2">Wisdom Dental</div>
-          </Row>
-          <Row>
-            <p className="description text-start">
-              Providing quality dental care for you and your family
-            </p>
-          </Row>
-          <Row>
-            <Link to='/login' className='text-start'>
-              <button className="primary-wrapper btn rounded shadow text-start">
-                <div className="primary">
-                  <div className="title-2">Book An Appointment</div>
-                </div>
-              </button>
-            </Link>
-            
-          </Row>
-        </Col>
-      </Row>
-      <Row className="my-5">
-        <Col md='6' className='pe-5 d-flex justify-contents-center'>
-          <div className="list">
-            <div className="container-2">
-              <Row>
-                <div className="title-3">Why Choose Us</div>
-
-              </Row>
-              <Row>
-                <p className="description">
-                  Reasons to trust us with your dental care
-                </p>
-              </Row>
-              <div className="list-2">
-                <Row>
-                  <div className="row">
-                    <Col>
-                      <div className="item ">
-                        <div className="frame-2">
-                          <div className="size-48"><FontAwesomeIcon icon={faBookmark} size='4x' /></div>
-                        </div>
-                        <div className="frame-3">
-                          <div className="title-4">Experienced Dentists</div>
-                          <div className="subtitle">Years of expertise</div>
-                        </div>
+          <Row className='h-50 align-items-center'>
+            <Col>
+              <div className="list">
+                <div className="container-2">
+                  <Row>
+                    <div className="title-3">Why Choose Us</div>
+                  </Row>
+                  <Row>
+                    <p className="description">
+                      Reasons to trust us with your dental care
+                    </p>
+                  </Row>
+                  <div className="list-2">
+                    <Row>
+                      <div className="row">
+                        <Col>
+                          <div className="item ">
+                            <div className="frame-2">
+                              <div className="size-48"><FontAwesomeIcon icon={faBookmark} size='4x' /></div>
+                            </div>
+                            <div className="frame-3">
+                              <div className="title-4">Experienced Dentists</div>
+                              <div className="subtitle">Years of expertise</div>
+                            </div>
+                          </div>
+                        </Col>
+                        <Col>
+                          <div className="item ">
+                            <div className="activity-wrapper">
+                              <div className="size-48" >
+                                <FontAwesomeIcon icon={faFileWaveform} size='4x' />
+                              </div>
+                            </div>
+                            <div className="frame-3">
+                              <div className="title-4">State-of-the-art Technology</div>
+                              <div className="subtitle">Advanced dental equipment</div>
+                            </div>
+                          </div>
+                        </Col>
                       </div>
-                    </Col>
-                    <Col>
-                      <div className="item ">
-                        <div className="activity-wrapper">
-                          <div className="size-48" >
-                            <FontAwesomeIcon icon={faFileWaveform} size='4x' />
+                    </Row>
+                    <Row>
+                        <div>
+                      <div className="item">
+                        <div className="frame-2">
+                          <div className="d-flex justify-content-center pt-3">
+                            <div className=""><FontAwesomeIcon icon={faStar} size='4x' /></div>
                           </div>
                         </div>
                         <div className="frame-3">
-                          <div className="title-4">State-of-the-art Technology</div>
-                          <div className="subtitle">Advanced dental equipment</div>
+                          <div className="title-4">Excellent Patient Care</div>
+                          <p className="subtitle">Your comfort is our priority</p>
                         </div>
                       </div>
-                    </Col>
-                  </div>
-                </Row>
-                <Row>
-                    <div>
-                  <div className="item">
-                    <div className="frame-2">
-                      <div className="d-flex justify-content-center pt-3">
-                        <div className=""><FontAwesomeIcon icon={faStar} size='4x' /></div>
-                      </div>
                     </div>
-                    <div className="frame-3">
-                      <div className="title-4">Excellent Patient Care</div>
-                      <p className="subtitle">Your comfort is our priority</p>
-                    </div>
+                    </Row>
                   </div>
                 </div>
+              </div>
+            </Col>
+          </Row>
+        </Col>
+        <Col md='6'>
+          <Row className='h-50 align-items-center'>
+            <Col>
+              <div>
+                <Row>
+                  <div className="text-wrapper text-start pb-1">Welcome to</div>
+                </Row>
+                <Row>
+                  <div className="div gruppo-regular text-start pb-2">Wisdom Dental</div>
+                </Row>
+                <Row>
+                  <p className="description text-start">
+                    Providing quality dental care for you and your family
+                  </p>
+                </Row>
+                <Row>
+                  <Link to='/login' className='text-start'>
+                    <button className="primary-wrapper btn rounded shadow text-start">
+                      <div className="primary">
+                        <div className="title-2">Book An Appointment</div>
+                      </div>
+                    </button>
+                  </Link>
                 </Row>
               </div>
-            </div>
-          </div>
+            </Col>
+          </Row>
+          <Row className="h-50 align-items-center">
+            <Col className='d-flex justify-content-center'>
+              <img
+                className='image-wrappe image img-fluid'
+                alt='home page image, girl smiling'
+                src={smile}
+              />
+            </Col>
+          </Row>
         </Col>
-        <Col md="6" className='d-flex align-items-center'>
-          <img
-            className='image-wrappe image img-fluid'
-            alt='home page image, girl smiling'
-            src={smile}
-          />
-        </Col>
+        <hr className='divider'></hr>
       </Row>
-      <div className="div-wrapper">
-        <div className="rounded shadow">
-          <Link to="/about">
-            <button type='button' className='btn shadow rounded btn-lg title-wrapper title-5'>Learn More About Us!</button>
-          </Link>
+
+      <Row className='align-items-stretch mt-5'>
+        <div className="div-wrapper mt-5">
+          <div className="rounded shadow">
+            <Link to="/about">
+              <button type='button' className='btn shadow rounded btn-lg title-wrapper title-5'>Learn More About Us!</button>
+            </Link>
+          </div>
         </div>
-      </div>
+      </Row>
     </div>
   )
 }
