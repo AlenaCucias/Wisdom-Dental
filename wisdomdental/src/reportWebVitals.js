f19bb847d9ed15353053731cc544641b0f8303eb
ishkaramk <ishkaramk@gmail.com>
2024-12-02 11:55:21 -0800

    Vital comments

--- wisdomdental/src/reportWebVitals.js
+++ wisdomdental/src/reportWebVitals.js
@@ -1,11 +1,12 @@
+//function to measure and report web performance metrics
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
+      getCLS(onPerfEntry); //CLS: Cumulative Layout Shift
+      getFID(onPerfEntry); //FID: First Input Delay
+      getFCP(onPerfEntry); //FCP: First Contentful Paint
+      getLCP(onPerfEntry); //LCP: Largest Contentful Paint
+      getTTFB(onPerfEntry);//TTFB:  Time to First Byte
     });
   }
 };
